28725bbb2bda518026f12b6c429882ab65bbdc2b
chaaaaaarlie <cflowers@varickmm.com>
2017-02-18 21:34:08 -0500

    Initial commit v2

--- signmeup.sh
+++ signmeup.sh
@@ -2,17 +2,21 @@
 
 
 # Common variables
-USER_AGENT="Mozilla/5.0 (Windows NT 6.1; rv:50.0) Gecko/20100101 Firefox/50.0"
+USER_AGENT="Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.85 Mobile Safari/537.36"
 URL=https://gop.com/survey/mainstream-media-accountability-survey/
 
 WORDS_FILE=/usr/share/dict/words
 NAMES_FILE=/usr/share/dict/propernames
 
 until [ 'trump' = 'jailed' ]; do
-	
+    
+
 	#Generate 32-bit random integer
 	RANDO=`od   -An -N4 -tu1 < /dev/urandom | sed 's/ //g'` 
+	
+	WK_VER=`expr $RANDOM % 3`.`expr $RANDOM % 5`.`expr $RANDOM % 10`
 
+	USER_AGENT="Mozilla/5.0 (iPhone; CPU iPhone OS 10_2_1 like Mac OS X) AppleWebKit/60"$WK_VER" (KHTML, like Gecko) Version/10.0 Mobile/14D27 Safari/60"$WK_VER
 	# Get # of lines in word/name files
 	NAMES_COUNT=`wc -l $NAMES_FILE | awk '{print $1}'`
 	WORDS_COUNT=`wc -l $WORDS_FILE | awk '{print $1}'`
@@ -23,8 +27,9 @@ until [ 'trump' = 'jailed' ]; do
 
 	#Spit out random name and word; capitalize word
 	NAME=`sed -n "$NAME_LN"p $NAMES_FILE`
-	WORD=`sed -n "$WORD_LN"p $WORDS_FILE |  awk '{print(toupper(substr($1,1,1)))substr($1,2,length($1) - 1)}'`
+	WORD=`sed -n "$WORD_LN"p $WORDS_FILE`
 
+	WORD_UPPER=`echo $WORD | awk '{print(toupper(substr($1,1,1)))substr($1,2,length($1) - 1)}'`
 	# email = last name ($WORD) + number + gmail.com
 	EMAIL=`echo $WORD$NAME_LN@gmail.com`
 	ZIP=`awk 'BEGIN{srand();printf("%05d", int(rand()*99998 )+ 1) }'`
@@ -53,7 +58,7 @@ until [ 'trump' = 'jailed' ]; do
 	done
 
 	# Create curl call
-	CMD="curl -A '$USER_AGENT' -F 'full_name=$NAME $WORD' -F 'email=$EMAIL' -F  'postal_code=$ZIP' $QBODY $URL"
+	CMD="curl -A '$USER_AGENT' -F 'full_name=$NAME $WORD_UPPER' -F 'email=$EMAIL' -F  'postal_code=$ZIP' $QBODY $URL"
 
 	# Uncomment to test output
 	echo $CMD
